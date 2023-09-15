a36ea4c6ac704202657dba76ab3ac06aa0ceb818
sujit <ssaroj9519@gmail.com>
2023-09-15 22:29:54 +0530

    First commit

---
+++ sumofdigitaftercon.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {number}
+ */
+var getLucky = function(s, k) {
+    let numStr = ''
+    for (let i=0; i<s.length;i++){
+        numStr += (s.charCodeAt(i)-96).toString();
+    }
+
+    while(k!=0){
+        let sum = 0
+        for(let i=0; i<numStr.length;i++){
+            sum += Number(numStr[i]);
+
+        }
+        numStr = sum.toString();
+        k--
+
+    }
+    return numStr
+
+    
+};
