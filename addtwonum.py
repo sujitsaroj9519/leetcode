8a7e123ccc1d135fcf420802b989d98d1b27e1e7
sujit <ssaroj9519@gmail.com>
2023-10-28 20:17:47 +0530

    First commit

---
+++ addtwonum.py
@@ -0,0 +1,12 @@
+class Solution:
+    def sum(self, num1: int, num2: int) -> int:
+
+
+        return num1 + num2
+        
+obj = Solution()
+print(obj.sum(12,5))
+# Input: num1 = 12, num2 = 5
+# Output: 17
+# Input: num1 = -10, num2 = 4
+# Output: -6
